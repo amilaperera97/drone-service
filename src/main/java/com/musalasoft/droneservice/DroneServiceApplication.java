8d8fb738118584bbce7c1f134c2c730227248926
Amila Perera <amilaperera97@gmail.com>
2022-09-09 19:07:41 +0100

    Code refactor

--- src/main/java/com/musalasoft/droneservice/DroneServiceApplication.java
+++ src/main/java/com/musalasoft/droneservice/DroneServiceApplication.java
@@ -1,12 +1,8 @@
 package com.musalasoft.droneservice;
 
-import com.musalasoft.droneservice.preload.PreLoadInitialData;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableWebMvc
